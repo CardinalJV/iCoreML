e0a1ed29cc68920780596500051832446dff4c29
CardinalJV <jessy.viranaiken@icloud.com>
2024-10-08 18:48:34 +0200

    Initial Commit

---
+++ iCoreML/iCoreMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCoreMLApp.swift
+//  iCoreML
+//
+//  Created by Jessy Viranaiken on 08/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct iCoreMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
